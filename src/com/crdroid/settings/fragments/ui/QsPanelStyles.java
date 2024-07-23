AlphaDroid-Project/packages_apps_AlphaSettings

Alpha styles code optimization

src/com/alpha/settings/fragments/qs/QsPanelStyles.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2023 crDroid Android Project
+ * Copyright (C) 2024 AlphaDroid
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,197 +17,17 @@
 
 package com.alpha.settings.fragments.qs;
 
-import android.content.ContentResolver;
-import android.content.Context;
-import android.content.res.Resources;
-import android.content.pm.PackageManager;
-import android.graphics.drawable.AnimationDrawable;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.os.UserHandle;
-import android.provider.SearchIndexableResource;
-import android.provider.Settings;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.Gravity;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.FrameLayout;
-import android.widget.TextView;
-import android.text.TextUtils;
-import androidx.preference.PreferenceViewHolder;
-import android.view.ViewGroup.LayoutParams;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.RecyclerView.ViewHolder;
-import androidx.recyclerview.widget.RecyclerView;
-import androidx.preference.Preference;
-import androidx.preference.Preference.OnPreferenceChangeListener;
-import androidx.preference.PreferenceScreen;
-
-import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.alpha.settings.fragments.AlphaStylesFragment;
 import com.android.settings.R;
-import com.android.settings.search.BaseSearchIndexProvider;
-import com.android.settingslib.search.Indexable;
-import com.android.settings.SettingsPreferenceFragment;
 
-import com.bumptech.glide.Glide;
-
-import com.android.internal.util.crdroid.ThemeUtils;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Arrays;
-
-import org.json.JSONObject;
-import org.json.JSONException;
-
-public class QsPanelStyles extends SettingsPreferenceFragment {
-
-    private RecyclerView mRecyclerView;
-    private ThemeUtils mThemeUtils;
-    private String mCategory = "android.theme.customization.qs_style";
-
-    private List<String> mPkgs;
+public class QsPanelStyles extends AlphaStylesFragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        setCategory("android.theme.customization.qs_style");
+        String title = getContext().getString(R.string.theme_customization_qs_style_title);
+        setTitle(title);
         super.onCreate(savedInstanceState);
-        getActivity().setTitle(R.string.theme_customization_qs_style_title);
-
-        mThemeUtils = new ThemeUtils(getActivity());
-        mPkgs = mThemeUtils.getOverlayPackagesForCategory(mCategory, "com.android.systemui");
-    }
-
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-            @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(
-                R.layout.item_view, container, false);
-
-        mRecyclerView = (RecyclerView) view.findViewById(R.id.recycler_view);
-        GridLayoutManager gridLayoutManager = new GridLayoutManager(getActivity(), 1);
-        mRecyclerView.setLayoutManager(gridLayoutManager);
-        Adapter mAdapter = new Adapter(getActivity());
-        mRecyclerView.setAdapter(mAdapter);
-
-        return view;
-    }
-
-    @Override
-    public int getMetricsCategory() {
-        return MetricsEvent.ALPHA;
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-    }
-
-    public class Adapter extends RecyclerView.Adapter<Adapter.CustomViewHolder> {
-        Context context;
-        String mSelectedPkg;
-        String mAppliedPkg;
-
-        public Adapter(Context context) {
-            this.context = context;
-        }
-
-        @Override
-        public CustomViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.qs_panel_option, parent, false);
-            CustomViewHolder vh = new CustomViewHolder(v);
-            return vh;
-        }
-
-        @Override
-        public void onBindViewHolder(CustomViewHolder holder, final int position) {
-            String navPkg = mPkgs.get(position);
-
-            String currentPackageName = mThemeUtils.getOverlayInfos(mCategory, "com.android.systemui").stream()
-                .filter(info -> info.isEnabled())
-                .map(info -> info.packageName)
-                .findFirst()
-                .orElse("com.android.systemui");
-
-            holder.name.setText("com.android.systemui".equals(navPkg) ? "Default" : getLabel(holder.name.getContext(), navPkg));
-
-            holder.name.setTextSize(24);
-
-            if (currentPackageName.equals(navPkg)) {
-                mAppliedPkg = navPkg;
-                if (mSelectedPkg == null) {
-                    mSelectedPkg = navPkg;
-                }
-            }
-
-            holder.itemView.setActivated(navPkg == mSelectedPkg);
-            holder.itemView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    updateActivatedStatus(mSelectedPkg, false);
-                    updateActivatedStatus(navPkg, true);
-                    mSelectedPkg = navPkg;
-                    enableOverlays(position);
-                }
-            });
-        }
-
-        @Override
-        public int getItemCount() {
-            return mPkgs.size();
-        }
-
-        public class CustomViewHolder extends RecyclerView.ViewHolder {
-            TextView name;
-            public CustomViewHolder(View itemView) {
-                super(itemView);
-                name = (TextView) itemView.findViewById(R.id.option_label);
-            }
-        }
-
-        private void updateActivatedStatus(String pkg, boolean isActivated) {
-            int index = mPkgs.indexOf(pkg);
-            if (index < 0) {
-                return;
-            }
-            RecyclerView.ViewHolder holder = mRecyclerView.findViewHolderForAdapterPosition(index);
-            if (holder != null && holder.itemView != null) {
-                holder.itemView.setActivated(isActivated);
-            }
-        }
-    }
-
-    public Drawable getDrawable(Context context, String pkg, String drawableName) {
-        try {
-            PackageManager pm = context.getPackageManager();
-            Resources res = pm.getResourcesForApplication(pkg);
-            int resId = res.getIdentifier(drawableName, "drawable", pkg);
-            return res.getDrawable(resId);
-        }
-        catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    public String getLabel(Context context, String pkg) {
-        PackageManager pm = context.getPackageManager();
-        try {
-            return pm.getApplicationInfo(pkg, 0)
-                    .loadLabel(pm).toString();
-        } catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
-        }
-        return pkg;
-    }
-
-    public void enableOverlays(int position) {
-        mThemeUtils.setOverlayEnabled(mCategory, mPkgs.get(position), "com.android.systemui");
     }
 }

LouisG1977/android_packages_apps_Personalizations

src/com/crdroid/settings/fragments/ui/QsPanelStyles.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2023 crDroid Android Project
+ * Copyright (C) 2024 AlphaDroid
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,197 +17,17 @@
 
 package com.crdroid.settings.fragments.ui;
 
-import android.content.ContentResolver;
-import android.content.Context;
-import android.content.res.Resources;
-import android.content.pm.PackageManager;
-import android.graphics.drawable.AnimationDrawable;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.os.UserHandle;
-import android.provider.SearchIndexableResource;
-import android.provider.Settings;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.Gravity;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.FrameLayout;
-import android.widget.TextView;
-import android.text.TextUtils;
-import androidx.preference.PreferenceViewHolder;
-import android.view.ViewGroup.LayoutParams;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.RecyclerView.ViewHolder;
-import androidx.recyclerview.widget.RecyclerView;
-import androidx.preference.Preference;
-import androidx.preference.Preference.OnPreferenceChangeListener;
-import androidx.preference.PreferenceScreen;
-
-import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.crdroid.settings.fragments.AlphaStylesFragment;
 import com.android.settings.R;
-import com.android.settings.search.BaseSearchIndexProvider;
-import com.android.settingslib.search.Indexable;
-import com.android.settings.SettingsPreferenceFragment;
 
-import com.bumptech.glide.Glide;
-
-import com.android.internal.util.crdroid.ThemeUtils;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Arrays;
-
-import org.json.JSONObject;
-import org.json.JSONException;
-
-public class QsPanelStyles extends SettingsPreferenceFragment {
-
-    private RecyclerView mRecyclerView;
-    private ThemeUtils mThemeUtils;
-    private String mCategory = "android.theme.customization.qs_style";
-
-    private List<String> mPkgs;
+public class QsPanelStyles extends AlphaStylesFragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        setCategory("android.theme.customization.qs_style");
+        String title = getContext().getString(R.string.theme_customization_qs_style_title);
+        setTitle(title);
         super.onCreate(savedInstanceState);
-        getActivity().setTitle(R.string.theme_customization_qs_style_title);
-
-        mThemeUtils = new ThemeUtils(getActivity());
-        mPkgs = mThemeUtils.getOverlayPackagesForCategory(mCategory, "com.android.systemui");
-    }
-
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-            @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(
-                R.layout.item_view, container, false);
-
-        mRecyclerView = (RecyclerView) view.findViewById(R.id.recycler_view);
-        GridLayoutManager gridLayoutManager = new GridLayoutManager(getActivity(), 1);
-        mRecyclerView.setLayoutManager(gridLayoutManager);
-        Adapter mAdapter = new Adapter(getActivity());
-        mRecyclerView.setAdapter(mAdapter);
-
-        return view;
-    }
-
-    @Override
-    public int getMetricsCategory() {
-        return MetricsEvent.CRDROID_SETTINGS;
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-    }
-
-    public class Adapter extends RecyclerView.Adapter<Adapter.CustomViewHolder> {
-        Context context;
-        String mSelectedPkg;
-        String mAppliedPkg;
-
-        public Adapter(Context context) {
-            this.context = context;
-        }
-
-        @Override
-        public CustomViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.qs_panel_option, parent, false);
-            CustomViewHolder vh = new CustomViewHolder(v);
-            return vh;
-        }
-
-        @Override
-        public void onBindViewHolder(CustomViewHolder holder, final int position) {
-            String navPkg = mPkgs.get(position);
-
-            String currentPackageName = mThemeUtils.getOverlayInfos(mCategory, "com.android.systemui").stream()
-                .filter(info -> info.isEnabled())
-                .map(info -> info.packageName)
-                .findFirst()
-                .orElse("com.android.systemui");
-
-            holder.name.setText("com.android.systemui".equals(navPkg) ? "Default" : getLabel(holder.name.getContext(), navPkg));
-
-            holder.name.setTextSize(24);
-
-            if (currentPackageName.equals(navPkg)) {
-                mAppliedPkg = navPkg;
-                if (mSelectedPkg == null) {
-                    mSelectedPkg = navPkg;
-                }
-            }
-
-            holder.itemView.setActivated(navPkg == mSelectedPkg);
-            holder.itemView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    updateActivatedStatus(mSelectedPkg, false);
-                    updateActivatedStatus(navPkg, true);
-                    mSelectedPkg = navPkg;
-                    enableOverlays(position);
-                }
-            });
-        }
-
-        @Override
-        public int getItemCount() {
-            return mPkgs.size();
-        }
-
-        public class CustomViewHolder extends RecyclerView.ViewHolder {
-            TextView name;
-            public CustomViewHolder(View itemView) {
-                super(itemView);
-                name = (TextView) itemView.findViewById(R.id.option_label);
-            }
-        }
-
-        private void updateActivatedStatus(String pkg, boolean isActivated) {
-            int index = mPkgs.indexOf(pkg);
-            if (index < 0) {
-                return;
-            }
-            RecyclerView.ViewHolder holder = mRecyclerView.findViewHolderForAdapterPosition(index);
-            if (holder != null && holder.itemView != null) {
-                holder.itemView.setActivated(isActivated);
-            }
-        }
-    }
-
-    public Drawable getDrawable(Context context, String pkg, String drawableName) {
-        try {
-            PackageManager pm = context.getPackageManager();
-            Resources res = pm.getResourcesForApplication(pkg);
-            int resId = res.getIdentifier(drawableName, "drawable", pkg);
-            return res.getDrawable(resId);
-        }
-        catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    public String getLabel(Context context, String pkg) {
-        PackageManager pm = context.getPackageManager();
-        try {
-            return pm.getApplicationInfo(pkg, 0)
-                    .loadLabel(pm).toString();
-        } catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
-        }
-        return pkg;
-    }
-
-    public void enableOverlays(int position) {
-        mThemeUtils.setOverlayEnabled(mCategory, mPkgs.get(position), "com.android.systemui");
     }
 }