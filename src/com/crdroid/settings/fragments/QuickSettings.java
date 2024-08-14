269ded2e0d8aad91228bedbcf958512942d24200
elpaablo <donopwhile1@gmail.com>
2024-08-14 10:46:25 +0100

    QuickSettings: Split Shade: exclude cutout overlay

--- src/com/crdroid/settings/fragments/QuickSettings.java
+++ src/com/crdroid/settings/fragments/QuickSettings.java
@@ -75,9 +75,6 @@ public class QuickSettings extends SettingsPreferenceFragment implements
     private static final String QS_SPLIT_SHADE_LAYOUT_CTG = "android.theme.customization.qs_landscape_layout";
     private static final String QS_SPLIT_SHADE_LAYOUT_PKG = "com.android.systemui.qs.landscape.split_shade_layout";
     private static final String QS_SPLIT_SHADE_LAYOUT_TARGET = "com.android.systemui";
-    private static final String QS_SPLIT_SHADE_CUTOUT_CTG = "android.theme.customization.qs_landscape_cutout";
-    private static final String QS_SPLIT_SHADE_CUTOUT_PKG = "android.landscape.split_shade_cutout";
-    private static final String QS_SPLIT_SHADE_CUTOUT_TARGET = "android";
 
     private ListPreference mShowBrightnessSlider;
     private ListPreference mBrightnessSliderPosition;
@@ -173,8 +170,7 @@ public class QuickSettings extends SettingsPreferenceFragment implements
     }
 
     private boolean isSplitShadeEnabled() {
-        return mThemeUtils.isOverlayEnabled(QS_SPLIT_SHADE_LAYOUT_PKG)
-            && mThemeUtils.isOverlayEnabled(QS_SPLIT_SHADE_CUTOUT_PKG);
+        return mThemeUtils.isOverlayEnabled(QS_SPLIT_SHADE_LAYOUT_PKG);
     }
 
     private void updateSplitShadeState(boolean enable) {
@@ -183,11 +179,6 @@ public class QuickSettings extends SettingsPreferenceFragment implements
                 QS_SPLIT_SHADE_LAYOUT_CTG,
                 enable ? QS_SPLIT_SHADE_LAYOUT_PKG : QS_SPLIT_SHADE_LAYOUT_TARGET,
                 QS_SPLIT_SHADE_LAYOUT_TARGET);
-
-        mThemeUtils.setOverlayEnabled(
-                QS_SPLIT_SHADE_CUTOUT_CTG,
-                enable ? QS_SPLIT_SHADE_CUTOUT_PKG : QS_SPLIT_SHADE_CUTOUT_TARGET,
-                QS_SPLIT_SHADE_CUTOUT_TARGET);
     }
 
     public static void reset(Context mContext) {
@@ -235,8 +226,6 @@ public class QuickSettings extends SettingsPreferenceFragment implements
         QsHeaderImageSettings.reset(mContext);
         ResourceUtils.updateOverlay(mContext, QS_SPLIT_SHADE_LAYOUT_CTG, QS_SPLIT_SHADE_LAYOUT_TARGET,
                 QS_SPLIT_SHADE_LAYOUT_TARGET);
-        ResourceUtils.updateOverlay(mContext, QS_SPLIT_SHADE_CUTOUT_CTG, QS_SPLIT_SHADE_CUTOUT_TARGET,
-                QS_SPLIT_SHADE_CUTOUT_TARGET);
     }
 
     private void updateAnimTileStyle(int tileAnimationStyle) {
